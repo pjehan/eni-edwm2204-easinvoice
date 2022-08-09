efcc6b0e408e6aad53da5951ac9eac2f5bc7b0d9
pjehan <pierre.jehan@gmail.com>
2022-08-09 11:34:27 +0200

    Création de la page d'accueil et de la page contact

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'fullname' => 'Pierre Jehan',
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('main/contact.html.twig');
+    }
+}
